7cb2ea563f773a9403cbc471d7be963cb06de6cf
frankhn <harfrank2@gmail.com>
2021-01-14 11:56:54 +0200

    back propagation

---
+++ backpropagation.py
@@ -0,0 +1,18 @@
+import torch
+
+x = torch.tensor(1.0)
+y = torch.tensor(2.0)
+
+w = torch.tensor(1.0, requires_grad=True)
+
+# forward pass and compute the loss
+y_hat = w * x
+loss = (y_hat - y)**2
+
+print(loss)
+
+#backword pass
+loss.backward()
+
+### update weights
+### next forward and backwardpass
